08e986300ed3bd95c4b09c9392b13720c6c5670c
nadir.belhaj <nadir.belhaj@student.rocva.nl>
2022-03-07 10:10:48 +0100

    antwoorden.sql

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,14 @@
 -- Opdracht 1 
+DELETE FROM players WHERE name = "David Silva"
 
 -- Opdracht 2 
+DELETE FROM players WHERE club = "Willem II"
 
 -- Opdracht 3
+DELETE FROM players WHERE club = "FC Barcelona" AND nationality = "belgium"
 
 -- Opdracht 4
+DELETE FROM players WHERE age >= "38"
 
 -- Opdracht 5
+DELETE FROM players WHERE id = "167905" OR id = "169595"
