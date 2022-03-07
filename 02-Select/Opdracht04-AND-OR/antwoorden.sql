443a225fd76675d2cf68dbed93668b94a180391d
nadir.belhaj <nadir.belhaj@student.rocva.nl>
2022-03-07 09:38:18 +0100

    antoorden.sql

--- 02-Select/Opdracht04-AND-OR/antwoorden.sql
+++ 02-Select/Opdracht04-AND-OR/antwoorden.sql
@@ -1,30 +1,44 @@
 -- Opdracht 1 
+SELECT * FROM players WHERE nationality = "Spain" AND club = "Chelsea"
 
 -- Opdracht 2 
+SELECT * FROM players WHERE nationality = "Spain" AND wage = "15000" AND age = "17"
 
 -- Opdracht 3
+SELECT * FROM players WHERE club = "Liverpool" AND age > "20"
 
 -- Opdracht 4
+SELECT * FROM players WHERE club = "Ajax" AND nationality = "Netherlands"
 
 -- Opdracht 5
+SELECT * FROM players WHERE club = "Ajax" AND nationality != "Netherlands"
 
 -- Opdracht 6 
+SELECT name, age FROM players WHERE club = "AZ Alkmaar"
 
 -- Opdracht 7 
+SELECT name, age, club FROM players WHERE club = "AZ Alkmaar"
 
 -- Opdracht 8
+SELECT name, wage FROM players WHERE club = "Manchester City" AND nationality = "brazil"
 
 -- Opdracht 9
+SELECT name FROM players WHERE age = "30" AND wage < "10000"
 
 -- Opdracht 10
+SELECT name, age FROM players WHERE nationality = "Spain" OR nationality = "Portugal"
 
 -- Opdracht 11
+SELECT name, age, club FROM players WHERE club = "Chelsea" OR nationality = "Portugal"
 
 -- Opdracht 12
+SELECT name, club FROM players WHERE age > "40" OR wage > "10000"
 
 -- Opdracht 13
+SELECT * FROM players WHERE nationality = "Netherlands" AND club = "Ajax" OR club = "FC Utrecht"
 
 -- Opdracht 14
+SELECT * FROM players WHERE nationality = "England" AND wage > "100000" AND age > "20"
 
 -- Opdracht 15
-
+SELECT age, name, nationality FROM players WHERE nationality = "Argentina" OR nationality = "Brazil" AND age > "25"
--- 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
+++ 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
@@ -1,19 +1,29 @@
 -- Opdracht 1 
+SELECT Max(wage) As hoogste_loon_speler_fc_utrecht FROM players WHERE club = "FC Utrecht"
 
 -- Opdracht 2 
+SELECT ROUND(wage) As gemiddelde_inkomen_van_alle_spelers FROM players WHERE wage
 
 -- Opdracht 3
+SELECT SUM(wage) As loon_van_alle_spelers_van_fc_utrecht FROM players WHERE club = "FC Utrecht"
 
 -- Opdracht 4
+SELECT COUNT(name) As aantal_speler FROM players WHERE club = "Manchester City" OR club = "Manchester United"
 
 -- Opdracht 5
+SELECT AVG(wage) As gemiddelde_salaris_nederlandse_spelers FROM players WHERE nationality = "Netherlands"
 
 -- Opdracht 6 
+SELECT AVG(wage) As gemiddelde_salaris_spelers_onder_20 FROM players WHERE age < "20"
 
 -- Opdracht 7 
+SELECT AVG(wage) As gemiddelde_salaris_spelers_boven_20 FROM players WHERE age > "20"
 
 -- Opdracht 8
+SELECT SUM(value) As waarde_van_chelsea_spelers FROM players WHERE club = "chelsea"
 
 -- Opdracht 9
+SELECT ROUND(age) As gemiddelde_leeftijd_van_alle_spelers FROM players WHERE age
 
 -- Opdracht 10
+SELECT club, SUM(wage) AS gemiddelde_inkomen_van_alle_liverpool_spelers, ROUND(value) As gemiddelde_waarde_van_alle_liverpool_spelers FROM players WHERE club = "liverpool";
